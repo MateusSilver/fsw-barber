f5490841683192096a9ce574a95077db38eecd3b
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-02-02 14:32:13 -0300

    feat: today date message with date-fns

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,17 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
 
 const Home = () => {
     return (
         <div>
             <Header />
-            <h2>Olá, Miguel!</h2>
-            <p>{}</p>
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold ">Olá, Miguel!</h2>
+                <p className="capitalize text-sm">{format(new Date(),"EEEE',' d 'de' MMMM 'de' yyyy", {
+                    locale: ptBR,
+                })}</p>
+            </div>
         </div>
     );
 }
