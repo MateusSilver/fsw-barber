bbf71049b59b9fcee9ff3fc9cf582364b423385b
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-02-02 14:13:54 -0300

    feat: add header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../_components/header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h2>Olá, Miguel!</h2>
+            <p>{}</p>
+        </div>
+    );
+}
+ 
+export default Home;
