3c48f6883eddf4b5cc54cd10a4d7c1dd6ec1de07
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-02-06 20:27:05 -0300

    feat: add AuthProvider component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
             <Header />
             <div className="max-w-[800px] mx-auto">
                 <div className="px-5 pt-5">
-                    <h2 className="text-xl font-bold ">Olá, Miguel!</h2>
+                <h2 className="text-xl font-bold ">Olá, Miguel!</h2>
                     <p className="capitalize text-sm">{format(new Date(),"EEEE',' d 'de' MMMM 'de' yyyy", {
                         locale: ptBR,
                     })}</p>
