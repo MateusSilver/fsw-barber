b19e05103178b560a192bfc890527fbaa91ee5d3
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-02-06 20:30:10 -0300

    feat: add authprovider component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { signIn } from "next-auth/react";
 
 const Header = () => {
+    const handleLoginClick = async () => {
+        await signIn();
+    }
     return (
         <Card>
             <CardContent className="p-5 flex flex-row justify-between items-center">
@@ -12,9 +17,12 @@ const Header = () => {
                     height={22}
                     width={120} 
                 />
-                <Button variant="outline" size="icon">
-                    <MenuIcon size={18} />
+                <Button onClick={handleLoginClick}>
+                    Login
                 </Button>
+                {/*<Button variant="outline" size="icon">
+                    <MenuIcon size={18} />
+                </Button>*/}
             </CardContent>
         </Card>
     );
